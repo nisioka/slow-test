91607a6daad61dfb2029706524d68a22b35dd468
nisioka <16322296+nisioka@users.noreply.github.com>
2024-02-04 17:43:55 +0900

    Initial commit

---
+++ src/main/java/com/example/slowtest/SlowTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.slowtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SlowTestApplication.class, args);
+  }
+
+}
